800b1a8874524b4280f7cbef3eb2e9d4e9fe259e
aamir140896h <aamir140896h@gmail.com>
2025-05-29 11:46:51 +0530

    new changes

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -3,8 +3,6 @@ import { updateClientSchema } from "../validation/validator.js";
 
 export const updateClient = async (req, res, next) => {
   try {
-    console.log(req.params.id);
-    console.log(req.body);
     const { error } = updateClientSchema.validate(req.body);
     if (error)
       return res.status(400).json({ message: error.details[0].message });
